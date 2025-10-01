e584ebc8755a0380913a8cfaca93bb27429717e1
neonmike <1850319300@qq.com>
2025-10-01 07:43:00 -0400

    upload shell base script

---
+++ centos-mirror-switcher.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+#    Copyright [2025] CCK
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+os_name="UnknownOS"
+os_version="UnknownVersion"
+os_plat=$(uname -m)
+
+sys_check() {
+	echo "权限检查......................................."
+	if [ -e /etc/os-release ]; then
+		. /etc/os-release # 读取系统发行版信息
+	fi
+	# 权限检查：确保用户是 root 或具有 sudo 权限
+	echo "检测系统名称：${ID},系统版本：${VERSION_ID}..........."
+	if [ "$(id -u)" -ne 0 ] && ! sudo -v >/dev/null 2>&1; then
+		echo "请切换为具有 sudo 权限的用户来开启脚本"
+		exit 1
+	fi
+	os_name=${ID}
+	os_version=${VERSION_ID}
+	if [[ "$os_name" == "UnknownOS" || "$os_version" == "UnknownVersion" ]]; then
+		echo "Unknown OS detected: os_name=$os_name, os_version=$os_version. Script exiting with status 1."
+		exit 1
+	fi
+	echo "just for Centos maintain china sofeware source "
+	echo "System_OS:${os_name} ;System_version:${os_version} ;"
+}
+# 提供对其他平台的源地址更新
+update_centos_othplat() {
+	if [[ "$1" == "centos" && ("$2" == "7" || "$2" == "8") ]]; then
+		sudo sed -e 's|^mirrorlist=|#mirrorlist=|g' \
+			-e 's|^#baseurl=http://mirror.centos.org/altarch/|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-altarch/|g' \
+			-e 's|^#baseurl=http://mirror.centos.org/$contentdir/|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-altarch/|g' \
+			-i.bak \
+			/etc/yum.repos.d/CentOS-*.repo
+		eles
+		echo " update centos failed ,Centos $1 ,version : $2 "
+	fi
+}
+
+update_centos_x86() {
+	if [[ "$1" == "centos" && "$2" == "7" ]]; then
+		sed -e "s|^mirrorlist=|#mirrorlist=|g" \
+			-e "s|^#baseurl=http://mirror.centos.org/centos/\$releasever|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/7.9.2009|g" \
+			-e "s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/7.9.2009|g" \
+			-i.bak \
+			/etc/yum.repos.d/CentOS-*.repo
+	elif [[ "$1" == "centos" && "$2" == "8" ]]; then
+		# CentOS 8
+		sed -e "s|^mirrorlist=|#mirrorlist=|g" \
+			-e "s|^#baseurl=http://mirror.centos.org/centos/\$releasever|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/8.5.2111|g" \
+			-e "s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/8.5.2111|g" \
+			-i.bak \
+			/etc/yum.repos.d/CentOS-*.repo
+	elif [[ "$1" == "centos" && "$2" == "6" ]]; then
+		#  Centos 6
+		sed -e "s|^mirrorlist=|#mirrorlist=|g" \
+			-e "s|^#baseurl=http://mirror.centos.org/centos/\$releasever|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/6.10|g" \
+			-e "s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/6.10|g" \
+			-i.bak \
+			/etc/yum.repos.d/CentOS-*.repo
+	elif [[ "$1" == "centos" && "$2" == "5" ]]; then
+		sed -e "s|^mirrorlist=|#mirrorlist=|g" \
+			-e "s|^#baseurl=http://mirror.centos.org/centos/\$releasever|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/5.11|g" \
+			-e "s|^#baseurl=http://mirror.centos.org/\$contentdir/\$releasever|baseurl=https://mirrors.tuna.tsinghua.edu.cn/centos-vault/5.11|g" \
+			-i.bak \
+			/etc/yum.repos.d/CentOS-*.repo
+	else
+		echo "only support x86_64 centos 6/7/8 version , fail udpate centos software source "
+	fi
+}
+
+update_centos() {
+	if [[ "$1" == "x86_64" ]]; then
+		update_centos_x86 "$2" "$3"
+	else
+		update_centos_othplat "$2" "$3"
+	fi
+}
+
+sys_check
+
+update_centos "$os_plat" "$os_name" "$os_version"
+
+yum makecache
